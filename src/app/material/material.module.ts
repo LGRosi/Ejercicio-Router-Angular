07c4f1c0795169b2dfd493f6eea21fcf19ff70c2
LGRosi <lrosi1996@gmail.com>
2022-03-20 00:54:29 -0300

    Actualización del Header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -12,8 +10,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatListModule,
-    MatToolbarModule
   ]
 })
 export class MaterialModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
+    NavbarComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MaterialModule
   ],
 })
 export class SharedModule { }
